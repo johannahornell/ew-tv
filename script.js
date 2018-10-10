40e110d33f5708b2d0505ca3078525026cd1a8c2
johannahornell <johanna.hornell95@gmail.com>
2018-10-10 16:09:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,162 @@
-// JavaScript source code
-google.charts.load('current', { 'packages': ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
+// Highchart Line
+$(function () {
+    var lineChart = Highcharts.chart('lineContainer', {
 
-// Draw the chart and set the chart values
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['User devices', 'Hours per Day'],
-        ['Desktop', 50],
-        ['Surfplatta', 20],
-        ['Mobil', 30],
-    ]);
+        title: {
+            text: 'Total Trafik',
+            style: {
+                color: "#fff",
+                fontSize: '1.5rem',
+                fontWeight: '300'
+            }
+        },
 
-    // Optional; add a title and set the width and height of the chart
-    var options = {
-        'title': 'Mobilfördelning',
-        'width': 550,
-        'height': 400,
-        colors: ['#008f99', '#00becc', '#3cf3ff']
-    };
+        subtitle: {
+            text: 'Topp 5',
+            style: {
+                color: "#fff",
+                fontSize: '1rem'
+            }
+        },
 
-    // Display the chart inside the <div> element with id="piechart"
-    var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
-    chart.draw(data, options);
-}
+        chart: {
+            style: {
+                fontFamily: 'Open Sans, sans-serif'
+            },
+            backgroundColor: '#213140',
+            color: '#fff'
+        },
+
+        yAxis: {
+            title: {
+                useHTML: true,
+                text: 'BesÃ¶kare',
+                style: {
+                    color: '#fff',
+                    fontSize: '1.2rem'
+                }
+            },
+            gridLineColor: '#fff',
+            labels: {
+                style: {
+                    color: '#fff',
+                    fontSize: '1rem'
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+        xAxis: {
+            type: 'datetime',
+            labels: {
+                style: {
+                    color: '#fff',
+                    fontSize: '1rem',
+                    marginTop: '10px'
+                }
+            }
+        },
+        legend: {
+            itemStyle: {
+                color: '#fff',
+                fontWeight: '400',
+            }
+        },
+
+        plotOptions: {
+            series: {
+                pointStart: Date.UTC(2018, 0, 1),
+                pointIntervalUnit: 'month',
+               
+            }
+        },
+
+        series: [{
+            name: 'Webbsida',
+            data: [399, 456, 567, 734, 765, 607, 565, 468],
+            color: '#3cf3ff',
+            type: 'spline',
+            marker: {
+                symbol: 'circle'
+            }
+        }, {
+            name: 'Webbsida',
+            data: [489, 527, 657, 565, 618, 572, 549, 686],
+            color: '#de7f32',
+            type: 'spline',
+            marker: {
+                symbol: 'circle'
+            }
+        }, {
+            name: 'Webbsida',
+            data: [435, 418, 378, 521, 445, 323, 454, 442],
+            color: '#ffd852',
+            type: 'spline',
+            marker: {
+                symbol: 'circle'
+            }
+        }, {
+            name: 'Webbsida',
+            data: [245, 301, 345, 264, 351, 366, 328, 395],
+            color: '#ce5f90',
+            type: 'spline',
+            marker: {
+                symbol: 'circle'
+            }
+        }, {
+            name: 'Webbsida',
+            data: [565, 657, 746, 582, 563, 637, 753, 575],
+            color: '#a8de32',
+            type: 'spline',
+            marker: {
+                symbol: 'circle'
+            }
+        }],
+
+    });
+});
+
+// Highchart Pie
+$(function () {
+
+    var pieChart = Highcharts.chart('container', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            height: 220
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Mobile',
+                y: 20,
+                color: '#8500b6',
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Desktop',
+                y: 80,
+                color: '#adadad',
+            }]
+        }]
+    });
+});
